05a9b6ef3781e728a5b7871fdc6461c7ce52aa5f
vucat2000 <cnikolai@earthlink.net>
2019-02-23 18:35:07 -0500

    version 3

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -139,7 +139,7 @@ $(document).ready(function() {
             }
             else {
               unansweredAnswers++;
-\            }
+            }
           }
           UpdateUserScore();
          }
